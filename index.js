f550996844520b9e5f7f4928308e820a5b5ec189
Yao Yao <LukeYao233@gmail.com>
2019-02-07 15:42:54 -0500

    arrangement

--- index.js
+++ index.js
@@ -5,5 +5,6 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import {name as appName} from './app.json';
 
-AppRegistry.registerComponent("Eternal Remembrance", () => App);
+AppRegistry.registerComponent(appName, () => App);
